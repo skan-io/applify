4946ca4669fdf5339dd486e8b3c80c7856684071
nickmanks <nick.manks@gmail.com>
2019-01-20 17:24:32 +1000

    fix(package): Moved dependencies

--- src/tasks.js
+++ src/tasks.js
@@ -1,13 +1,15 @@
 import chalk from 'chalk';
 import {initializePackageManager} from './package-manager';
-import {initializeReadme} from './readme';
+import {initializeBackendManager} from './backend-manager';
 import {initializeSourceControl} from './source-control';
+import {initializeReadme} from './readme';
 
 
 const taskFunctions = {
   initPackageManager: initializePackageManager,
   initReadme: initializeReadme,
-  initSourceControl: initializeSourceControl
+  initSourceControl: initializeSourceControl,
+  initAmplify: initializeBackendManager
 };
 
 export const runTasks = async (tasks)=> {
